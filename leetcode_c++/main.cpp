6817c057ea1c46aab0dfda2954577af943f2bd78
Lyudmila Sun <sunyuchen2014@gmail.com>
2020-04-16 21:47:53 +0800

    Restructure Python and C++

---
+++ leetcode_c++/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  by_c++
+//
+//  Created by Yuchen Sun on 3/5/20.
+//  Copyright © 2020 Yuchen Sun. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+int helloWorld()
+{
+    cout << "Hello, world!" << endl;
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    helloWorld();
+    return 0;
+}
